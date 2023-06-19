157ddfebb622756e2a2e2ba84dda43c7f770a32c
Aatish <83630847+atish-tech@users.noreply.github.com>
2023-06-19 20:00:40 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -12,33 +12,20 @@ class Solution {
 	void shortest_distance(vector<vector<int>>&matrix)
 	{
 	    for(int i=0; i<matrix.size(); i++)
-	    {
 	        for(int j=0; j<matrix.size(); j++)
-	        {
 	            if(matrix[i][j] == -1)
 	                matrix[i][j] = 1e9;
-	        }
-	    }
+	                
 	    for(int k =0; k<matrix.size(); k++)
-	    {
 	        for(int i = 0; i< matrix.size(); i++)
-	        {
 	            for(int j=0; j<matrix.size(); j++)
-	            {
 	                matrix[i][j] = min(matrix[i][j] , matrix[k][j] + matrix[i][k]);
-	            }
-	               
-	        }
-	    }
-	    
+	         
 	    for(int i=0; i<matrix.size(); i++)
-	    {
+
 	        for(int j=0; j<matrix.size(); j++)
-	        {
 	            if(matrix[i][j] == 1e9)
 	                matrix[i][j] = -1;
-	        }
-	    }
 	}
 };
 
