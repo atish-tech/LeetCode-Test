ffb1047523c7381c3cc26a718b248d63b3dca13b
Aatish <83630847+atish-tech@users.noreply.github.com>
2023-06-28 02:43:13 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
@@ -10,19 +10,11 @@ class Solution
     public:
     void sort012(int a[], int n)
     {
-        vector<int>zero;
-        vector<int>one;
-        vector<int>two;
-        
-        for(int i=0; i<n; i++)
-            if(a[i] == 0)   zero.push_back(0);
-            else if(a[i] == 1)    one.push_back(1);
-            else    two.push_back(2);
-        
-        int i = 0;
-        for(int &x : zero)  a[i++] = 0;
-        for(int &x : one)   a[i++] = 1;
-        for(int &x : two)   a[i++] = 2;
+        int low = 0 , mid = 0 , high = n-1;
+        while(mid <= high)
+            if(a[mid] == 0)     swap(a[low++] , a[mid++]);
+            else if(a[mid] == 1)    mid++;
+            else    swap(a[mid] , a[high--]);
     }
     
 };
